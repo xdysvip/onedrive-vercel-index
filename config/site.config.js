cd3d1128270d4440f79ebac6c30a5732479b843c
xdysvip <100066292+xdysvip@users.noreply.github.com>
2022-02-20 17:43:37 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "xdys cloud drive",
+  title: "兄弟影视云盘",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
   baseDirectory: '/cloud',
